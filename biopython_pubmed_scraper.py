e6272619a47255226329a188f7c638d7d39afbc4
i.iliev <i.t.iliev@armf.bg>
2024-02-18 14:22:28 +0200

    add clean functions for names and is_stored. Clean up examples and imports

--- biopython_pubmed_scraper.py
+++ biopython_pubmed_scraper.py
@@ -1,8 +1,7 @@
 import json
 
-import pandas as pd
-
 from Bio import Entrez
+from typing import Literal, Union, Any
 
 
 QUERY = "Ulcerative Colitis"
@@ -11,168 +10,101 @@ EMAIL = "iliqanew@email.com"
 with open("person_profiles.json", "rb") as f:
     STORED_PROFILES = json.loads(f.read())
 
-
-example = {
-    "_id": {"$oid": "5c6ad92e1fba1942ccb101c2"},
+AUTHOR_T = {
+    # "_id": {"$oid": "5c6ad92e1fba1942ccb101c2"},
     "firstName": None,
-    "middleName": None,
-    "lastName": "Tjalling W de Vries, dr.",
+    # "middleName": None,
+    "lastName": None,  # "Tjalling W de Vries, dr.",
     "affiliation": None,
-    "degrees": None,
-    "phone": "0031582863390",
-    "phoneExt": None,
-    "email": "tjalling.de.vries@znb.nl",
-    "roleSpecified": True,
-    "role": 0,
-    "roles": [1],
-    "personType": 0,
-    "name": "Tjalling W de Vries, dr.",
-    "formattedName": "Tjalling De Vries",
-    "isMarkedAsDuplicate": False,
-    "lastDuplicationCheck": {"$date": "2022-04-26T01:44:11.340Z"},
-    "personTypeFixDate": {"$date": {"$numberLong": "-62135596800000"}},
-    "createdOn": {"$date": {"$numberLong": "-62135596800000"}},
-    "debarmentData": None,
-    "formattedAddress": "Leeuwarden, Friesland, Netherlands",
-    "website": None,
-    "placeId": None,
-    "location": None,
-    "contactsDownloadedFrom": None,
-    "googleSERPProcessed": False,
-    "notRealPerson": False,
-    "npi": None,
-    "medicalSchool": None,
-    "specialties": None,
-    "affiliateHospitals": None,
-    "medicareProcessed": True,
-    "medicareProcessedFix": True,
-    "phonesInfo": [
-        {"trialId": "NCT03654508", "value": "0031582863390"},
-        {"trialId": "NCT02197780", "value": "0031 582 866 666"},
-    ],
-    "emailsInfo": [
-        {"trialId": "NCT03654508", "value": "tjalling.de.vries@znb.nl"},
-        {"trialId": "NCT02197780", "value": "Tjalling.de.Vries@ZNB.nl"},
-    ],
-    "emailsInfoUpdated": True,
-    "fDAInspections": None,
-    "normalizedEmails": ["TJALLING.DE.VRIES@ZNB.NL"],
-    "normalizedPhones": ["0031582863390", "0031582866666"],
-    "trials": ["NCT03654508", "NCT02197780"],
+    # "degrees": None,
+    # "phone": "0031582863390",
+    # "phoneExt": None,
+    # "email": "tjalling.de.vries@znb.nl",
+    # "roleSpecified": True,
+    # "role": 0,
+    # "roles": [1],
+    # "personType": 0,
+    # "name": "Tjalling W de Vries, dr.",
+    # "formattedName": "Tjalling De Vries",
+    # "isMarkedAsDuplicate": False,
+    # "lastDuplicationCheck": {"$date": "2022-04-26T01:44:11.340Z"},
+    # "personTypeFixDate": {"$date": {"$numberLong": "-62135596800000"}},
+    # "createdOn": {"$date": {"$numberLong": "-62135596800000"}},
+    # "debarmentData": None,
+    # "formattedAddress": "Leeuwarden, Friesland, Netherlands",
+    # "website": None,
+    # "placeId": None,
+    # "location": None,
+    # "contactsDownloadedFrom": None,
+    # "googleSERPProcessed": False,
+    # "notRealPerson": False,
+    # "npi": None,
+    # "medicalSchool": None,
+    # "specialties": None,
+    # "affiliateHospitals": None,
+    # "medicareProcessed": True,
+    # "medicareProcessedFix": True,
+    # "phonesInfo": [
+    #     {"trialId": "NCT03654508", "value": "0031582863390"},
+    #     {"trialId": "NCT02197780", "value": "0031 582 866 666"},
+    # ],
+    # "emailsInfo": [
+    #     {"trialId": "NCT03654508", "value": "tjalling.de.vries@znb.nl"},
+    #     {"trialId": "NCT02197780", "value": "Tjalling.de.Vries@ZNB.nl"},
+    # ],
+    # "emailsInfoUpdated": True,
+    # "fDAInspections": None,
+    # "normalizedEmails": ["TJALLING.DE.VRIES@ZNB.NL"],
+    # "normalizedPhones": ["0031582863390", "0031582866666"],
+    # "trials": ["NCT03654508", "NCT02197780"],
+    "isStored": None,
 }
 
-example_2 = {
-    "_id": {"$oid": "5dca5c0097b95704782610a1"},
-    "firstName": None,
-    "middleName": None,
-    "lastName": "Zehre Yuksel, MD PhD",
-    "affiliation": None,
-    "degrees": None,
-    "phone": "0031 546 69 31 20",
-    "phoneExt": None,
-    "email": "z.yuksel@zgt.nl",
-    "roleSpecified": False,
-    "role": 0,
-    "roles": None,
-    "personType": 0,
-    "name": "Zehre Yuksel, MD PhD",
-    "formattedName": "Zehre Yuksel",
-    "isMarkedAsDuplicate": False,
-    "lastDuplicationCheck": {"$date": "2022-05-03T06:52:49.737Z"},
-    "personTypeFixDate": {"$date": {"$numberLong": "-62135596800000"}},
-    "createdOn": {"$date": {"$numberLong": "-62135596800000"}},
-    "debarmentData": None,
-    "formattedAddress": "Almelo, Netherlands",
-    "website": None,
-    "placeId": None,
-    "location": None,
-    "contactsDownloadedFrom": None,
-    "googleSERPProcessed": False,
-    "notRealPerson": False,
-    "npi": None,
-    "medicalSchool": None,
-    "specialties": None,
-    "affiliateHospitals": None,
-    "medicareProcessed": True,
-    "medicareProcessedFix": True,
-    "phonesInfo": [{"trialId": "NCT02197780", "value": "0031 546 69 31 20"}],
-    "emailsInfo": [{"trialId": "NCT02197780", "value": "z.yuksel@zgt.nl"}],
-    "emailsInfoUpdated": True,
-    "fDAInspections": None,
-    "normalizedEmails": ["Z.YUKSEL@ZGT.NL"],
-    "normalizedPhones": ["0031546693120"],
-    "trials": ["NCT02197780"],
-}
+
+def clean_names(
+    fore_name: Union[str, None], last_name: Union[str, None]
+) -> dict[Literal["firstName", "lastName"], Union[str, None]]:
+    # apply data cleaning logic here
+
+    return {"firstName": fore_name, "lastName": last_name}
+
+
+def clean_is_stored(author: dict[str, Any]) -> dict[Literal["isStored"], bool]:
+    fn = author["firstName"]
+    ln = author["lastName"]
+
+    _first_name = fn + " " if fn else ""
+    _last_name = ln if ln else ""
+
+    is_stored = _first_name + _last_name in [
+        profile["lastName"].split(",")[0]
+        for profile in STORED_PROFILES
+        if not profile["lastName"] is None
+    ]
+
+    return {"isStored": is_stored}
 
 
 def parse_studies(raw_studies):
     for raw_study in raw_studies:
         raw_authors = raw_study["MedlineCitation"]["Article"].get("AuthorList") or []
         for raw_author in raw_authors:
-            first_name = raw_author.get("ForeName")
-            last_name = raw_author.get("LastName")
+            author = AUTHOR_T.copy()
+
+            cleaned_names = clean_names(
+                raw_author.get("ForeName"), raw_author.get("LastName")
+            )
+            first_name = cleaned_names["firstName"]
+            last_name = cleaned_names["lastName"]
             if first_name is last_name is None:
                 continue
-            _first_name = first_name + " " if first_name else ""
-            _last_name = last_name if last_name else ""
-            is_stored = _first_name + _last_name in [
-                profile["lastName"].split(",")[0]
-                for profile in STORED_PROFILES
-                if not profile["lastName"] is None
-            ]
-            author = {
-                # "_id": {"$oid": "5c6ad92e1fba1942ccb101c2"},
-                "firstName": first_name,
-                # "middleName": None,
-                "lastName": last_name,  # "Tjalling W de Vries, dr.",
-                "affiliation": [
-                    str(raw_affiliation["Affiliation"])
-                    for raw_affiliation in raw_author.get("AffiliationInfo")
-                ]
-                or None,
-                # "degrees": None,
-                # "phone": "0031582863390",
-                # "phoneExt": None,
-                # "email": "tjalling.de.vries@znb.nl",
-                # "roleSpecified": True,
-                # "role": 0,
-                # "roles": [1],
-                # "personType": 0,
-                # "name": "Tjalling W de Vries, dr.",
-                # "formattedName": "Tjalling De Vries",
-                # "isMarkedAsDuplicate": False,
-                # "lastDuplicationCheck": {"$date": "2022-04-26T01:44:11.340Z"},
-                # "personTypeFixDate": {"$date": {"$numberLong": "-62135596800000"}},
-                # "createdOn": {"$date": {"$numberLong": "-62135596800000"}},
-                # "debarmentData": None,
-                # "formattedAddress": "Leeuwarden, Friesland, Netherlands",
-                # "website": None,
-                # "placeId": None,
-                # "location": None,
-                # "contactsDownloadedFrom": None,
-                # "googleSERPProcessed": False,
-                # "notRealPerson": False,
-                # "npi": None,
-                # "medicalSchool": None,
-                # "specialties": None,
-                # "affiliateHospitals": None,
-                # "medicareProcessed": True,
-                # "medicareProcessedFix": True,
-                # "phonesInfo": [
-                #     {"trialId": "NCT03654508", "value": "0031582863390"},
-                #     {"trialId": "NCT02197780", "value": "0031 582 866 666"},
-                # ],
-                # "emailsInfo": [
-                #     {"trialId": "NCT03654508", "value": "tjalling.de.vries@znb.nl"},
-                #     {"trialId": "NCT02197780", "value": "Tjalling.de.Vries@ZNB.nl"},
-                # ],
-                # "emailsInfoUpdated": True,
-                # "fDAInspections": None,
-                # "normalizedEmails": ["TJALLING.DE.VRIES@ZNB.NL"],
-                # "normalizedPhones": ["0031582863390", "0031582866666"],
-                # "trials": ["NCT03654508", "NCT02197780"],
-                "is_stored": is_stored,
-            }
+
+            author.update({"firstName": first_name, "lastName": last_name})
+
+            is_stored = clean_is_stored(author)
+
+            author.update(is_stored)
+
             yield author
 
 
@@ -222,5 +154,5 @@ def paginate_publicaitons_ids(query: str, batch_size: int = 9999):
         studies_id_list = studies_ids["idlist"]
 
 
-for author in paginate_publicaitons_ids(QUERY, batch_size=9999):
+for author in paginate_publicaitons_ids(QUERY, batch_size=10):
     print(author)
